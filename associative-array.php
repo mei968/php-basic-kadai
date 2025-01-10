ed058a70c5818d8f94e7ed9bbafd4c13996a8edc
mei968 <meinomadoka@gmail.com>
2025-01-10 16:56:20 +0900

    kadai_007をアップロード

---
+++ associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai007</title>
+</head>
+
+<body>
+  
+  <p>
+    <?php
+    $vegetables = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    // 連想配列の値を出力する
+    print_r($vegetables);
+    ?>
+  </p>
+
+</body>
+
+</html>
